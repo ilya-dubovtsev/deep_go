5eb509a1486fb2659b8e08cd5e6d9c05e169cd26
idubovtsev <dubovtsev@gmail.com>
2025-04-21 13:28:56 +0300

    h4 - 1

--- homework/maps/homewrok_test.go
+++ homework/maps/homewrok_test.go
@@ -9,34 +9,146 @@ import (
 
 // go test -v homework_test.go
 
+type TreeNode struct {
+	key    int
+	value  int
+	left   *TreeNode
+	right  *TreeNode
+	parent *TreeNode
+}
+
 type OrderedMap struct {
-	// need to implement
+	root *TreeNode
+	size int
 }
 
 func NewOrderedMap() OrderedMap {
-	return OrderedMap{} // need to implement
+	return OrderedMap{
+		root: nil,
+		size: 0,
+	}
 }
 
 func (m *OrderedMap) Insert(key, value int) {
-	// need to implement
+	newNode := &TreeNode{key: key, value: value}
+	if m.root == nil {
+		m.root = newNode
+		m.size++
+		return
+	}
+
+	current := m.root
+	for {
+		if key < current.key {
+			if current.left == nil {
+				current.left = newNode
+				newNode.parent = current
+				m.size++
+				return
+			}
+			current = current.left
+		} else if key > current.key {
+			if current.right == nil {
+				current.right = newNode
+				newNode.parent = current
+				m.size++
+				return
+			}
+			current = current.right
+		} else {
+			current.value = value
+			return
+		}
+	}
 }
 
 func (m *OrderedMap) Erase(key int) {
-	// need to implement
+	node := m.findNode(key)
+	if node == nil {
+		return
+	}
+
+	m.deleteNode(node)
+	m.size--
+}
+
+func (m *OrderedMap) findNode(key int) *TreeNode {
+	current := m.root
+	for current != nil {
+		if key == current.key {
+			return current
+		} else if key < current.key {
+			current = current.left
+		} else {
+			current = current.right
+		}
+	}
+	return nil
 }
 
 func (m *OrderedMap) Contains(key int) bool {
-	return false // need to implement
+	return m.findNode(key) != nil
 }
 
 func (m *OrderedMap) Size() int {
-	return 0 // need to implement
+	return m.size
 }
 
 func (m *OrderedMap) ForEach(action func(int, int)) {
-	// need to implement
+	m.inOrderTraversal(m.root, action)
 }
 
+func (m *OrderedMap) deleteNode(node *TreeNode) {
+	if node.left == nil && node.right == nil {
+		m.replaceNodeInParent(node, nil)
+	} else if node.left != nil && node.right != nil {
+		changeNode := m.minNode(node.right)
+		node.key = changeNode.key
+		node.value = changeNode.value
+		m.deleteNode(changeNode)
+	} else {
+		var child *TreeNode
+		if node.left != nil {
+			child = node.left
+		} else {
+			child = node.right
+		}
+		m.replaceNodeInParent(node, child)
+	}
+}
+
+func (m *OrderedMap) replaceNodeInParent(node, replacement *TreeNode) {
+	if node.parent != nil {
+		if node == node.parent.left {
+			node.parent.left = replacement
+		} else {
+			node.parent.right = replacement
+		}
+	} else {
+		m.root = replacement
+	}
+
+	if replacement != nil {
+		replacement.parent = node.parent
+	}
+}
+
+func (m *OrderedMap) minNode(node *TreeNode) *TreeNode {
+	current := node
+	for current.left != nil {
+		current = current.left
+	}
+	return current
+}
+
+func (m *OrderedMap) inOrderTraversal(node *TreeNode, action func(int, int)) {
+	if node == nil {
+		return
+	}
+	m.inOrderTraversal(node.left, action)
+	action(node.key, node.value)
+	m.inOrderTraversal(node.right, action)
+}
 func TestCircularQueue(t *testing.T) {
 	data := NewOrderedMap()
 	assert.Zero(t, data.Size())
