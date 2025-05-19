c7ccd0132c0ec5831bc5f3fae31446f947641651
idubovtsev <dubovtsev@gmail.com>
2025-05-19 16:59:41 +0300

    hw9 - 1

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,8 +19,58 @@ type Person struct {
 }
 
 func Serialize(person Person) string {
-	// need to implement
-	return ""
+	v := reflect.ValueOf(person)
+	t := v.Type()
+
+	var properties []string
+	fields := make(map[string]string)
+
+	for i := 0; i < v.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("properties")
+		if tag == "" {
+			continue
+		}
+
+		tagParts := strings.Split(tag, ",")
+		propName := tagParts[0]
+		omitempty := len(tagParts) > 1 && tagParts[1] == "omitempty"
+
+		fieldValue := v.Field(i)
+		zero := reflect.Zero(fieldValue.Type()).Interface()
+		current := fieldValue.Interface()
+
+		if omitempty && reflect.DeepEqual(current, zero) {
+			continue
+		}
+
+		var valueStr string
+		switch fieldValue.Kind() {
+		case reflect.String:
+			valueStr = fieldValue.String()
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			valueStr = fmt.Sprintf("%d", fieldValue.Int())
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			valueStr = fmt.Sprintf("%d", fieldValue.Uint())
+		case reflect.Bool:
+			valueStr = fmt.Sprintf("%t", fieldValue.Bool())
+		default:
+			valueStr = fmt.Sprintf("%v", current)
+		}
+
+		fields[propName] = valueStr
+		properties = append(properties, propName)
+	}
+
+	var builder strings.Builder
+	for i, prop := range properties {
+		if i > 0 {
+			builder.WriteString("\n")
+		}
+		builder.WriteString(fmt.Sprintf("%s=%s", prop, fields[prop]))
+	}
+
+	return builder.String()
 }
 
 func TestSerialization(t *testing.T) {
