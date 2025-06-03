37190277495409df752321a6b8d5a651e63f8667
idubovtsev <dubovtsev@gmail.com>
2025-06-03 22:31:54 +0300

    hw12 - 1

--- homework/goroutines_and_scheduler/homework_test.go
+++ homework/goroutines_and_scheduler/homework_test.go
@@ -12,25 +12,102 @@ type Task struct {
 }
 
 type Scheduler struct {
-	// need to implement
+	tasks   []Task
+	taskMap map[int]int // Отображение ID задачи на индекс в куче
 }
 
-func NewScheduler() Scheduler {
-	// need to implement
-	return Scheduler{}
+func NewScheduler() *Scheduler {
+	return &Scheduler{
+		taskMap: make(map[int]int),
+	}
 }
 
 func (s *Scheduler) AddTask(task Task) {
-	// need to implement
+	if _, exists := s.taskMap[task.Identifier]; exists {
+		return
+	}
+
+	s.tasks = append(s.tasks, task)
+	index := len(s.tasks) - 1
+	s.taskMap[task.Identifier] = index
+
+	s.siftUp(index)
 }
 
 func (s *Scheduler) ChangeTaskPriority(taskID int, newPriority int) {
-	// need to implement
+	index, exists := s.taskMap[taskID]
+	if !exists {
+		return
+	}
+
+	oldPriority := s.tasks[index].Priority
+	s.tasks[index].Priority = newPriority
+
+	if newPriority > oldPriority {
+		s.siftUp(index)
+	} else if newPriority < oldPriority {
+		s.siftDown(index)
+	}
 }
 
 func (s *Scheduler) GetTask() Task {
-	// need to implement
-	return Task{}
+	if len(s.tasks) == 0 {
+		return Task{Identifier: -1}
+	}
+
+	maxTask := s.tasks[0]
+	delete(s.taskMap, maxTask.Identifier)
+
+	lastIndex := len(s.tasks) - 1
+	if lastIndex > 0 {
+		s.tasks[0] = s.tasks[lastIndex]
+		s.taskMap[s.tasks[0].Identifier] = 0
+	}
+	s.tasks = s.tasks[:lastIndex]
+
+	if len(s.tasks) > 0 {
+		s.siftDown(0)
+	}
+
+	return maxTask
+}
+
+func (s *Scheduler) siftUp(index int) {
+	for index > 0 {
+		parent := (index - 1) / 2
+		if s.tasks[parent].Priority >= s.tasks[index].Priority {
+			break
+		}
+		s.swap(index, parent)
+		index = parent
+	}
+}
+
+func (s *Scheduler) siftDown(index int) {
+	size := len(s.tasks)
+	for {
+		left := 2*index + 1
+		right := 2*index + 2
+		largest := index
+
+		if left < size && s.tasks[left].Priority > s.tasks[largest].Priority {
+			largest = left
+		}
+		if right < size && s.tasks[right].Priority > s.tasks[largest].Priority {
+			largest = right
+		}
+		if largest == index {
+			break
+		}
+		s.swap(index, largest)
+		index = largest
+	}
+}
+
+func (s *Scheduler) swap(i, j int) {
+	s.tasks[i], s.tasks[j] = s.tasks[j], s.tasks[i]
+	s.taskMap[s.tasks[i].Identifier] = i
+	s.taskMap[s.tasks[j].Identifier] = j
 }
 
 func TestTrace(t *testing.T) {
@@ -56,7 +133,7 @@ func TestTrace(t *testing.T) {
 	scheduler.ChangeTaskPriority(1, 100)
 
 	task = scheduler.GetTask()
-	assert.Equal(t, task1, task)
+	assert.Equal(t, task1.Identifier, task.Identifier)
 
 	task = scheduler.GetTask()
 	assert.Equal(t, task3, task)
